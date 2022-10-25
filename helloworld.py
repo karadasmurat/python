73a09020c228558cee6acd0acefdeedc04dc19aa
karadasmurat <karadasmurat@gmail.com>
2022-10-25 20:12:12 +0300

    how to run

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
 # run 
 # $ python helloworld.py 
+
+# $ which python
+# /usr/local/bin/python
+
+# $ ls -l /usr/local/bin/python
+# lrwxr-xr-x  1 root  wheel  65 Oct 25 19:57 /usr/local/bin/python -> /Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10
+
+
 print("hello, there!")
