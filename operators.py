2418b5c013df34280d089490174cd11d07481aee
karadasmurat <karadasmurat@gmail.com>
2022-10-27 16:05:51 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,9 @@
+a = 3 + 2       # Add
+a += 10
+b = 4 - 1       # Subtract
+c = 2 * 3       # Multiply
+d = 5 / 2       # Divide
+e = 2 ** 10     # Exponent
+f = 5 % 2       # Modulo - remainder after a division
+
+print(f"{a=} {b=} {c=} {d=} {e=} {f=}") # a=15 b=3 c=6 d=2.5 e=1024 f=1
