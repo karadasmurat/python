5b99936f3e8ecb5664b6dc2311f73d7babde0c8a
karadasmurat <karadasmurat@gmail.com>
2022-10-27 16:11:11 +0300

    floor division

--- operators.py
+++ operators.py
@@ -2,8 +2,9 @@ a = 3 + 2       # Add
 a += 10
 b = 4 - 1       # Subtract
 c = 2 * 3       # Multiply
-d = 5 / 2       # Divide
+d1 = 17 / 3     # Divide
+d2 = 17 // 3    # Divide - floor division discards the fractional part
 e = 2 ** 10     # Exponent
 f = 5 % 2       # Modulo - remainder after a division
 
-print(f"{a=} {b=} {c=} {d=} {e=} {f=}") # a=15 b=3 c=6 d=2.5 e=1024 f=1
+print(f"{a=} {b=} {c=} {d1=} {d2=} {e=} {f=}") # a=15 b=3 c=6 d1=5.666666666666667 d2=5 e=1024 f=1
